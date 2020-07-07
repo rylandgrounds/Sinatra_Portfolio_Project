1dadf6c749dbaad5a395aa65bd6ae421bace49ff
rylandgrounds <rylandgrounds@gmail.com>
2020-07-07 12:24:10 -0500

    indentation and quality of life change

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,11 +27,13 @@ class ApplicationController < Sinatra::Base
         redirect to '/'
     end
   end
+  
   def redirect_if_not_logged_in
     if !logged_in?
       redirect "/login"
     end
   end
+
   def logout!
       if session[:username] != nil
         session.destroy
@@ -45,24 +47,23 @@ class ApplicationController < Sinatra::Base
      @current_user ||= User.find_by(:username => session[:username]) if session[:username]
     end
   end
-
-
-    def get_data(name)
-      url = "http://api.jikan.moe/v3/search/anime?q=#{name}&limit=5"
-      #get data from jikan api for editing
-      uri = URI.parse(url)
-      response = Net::HTTP.get_response(uri)
-      response.body
-    end
+    
+  def get_data(name)
+    url = "http://api.jikan.moe/v3/search/anime?q=#{name}&limit=5"
+    #get data from jikan api for editing
+    uri = URI.parse(url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
   
-    def edit_data(name)
-      animes = []
-      #edit it down to desired information and save it
-      data = JSON.parse(self.get_data(name))
-      results = data.fetch("results")
-      results.each do |result|
-      animes <<  Anime.new(:mal_id => result["mal_id"], :title => result["title"], :rated => result["rated"], :synopsis => result["synopsis"], :url => result["url"], :score => result["score"], :image_url => result["image_url"],:airing=> result["airing"], :episodes => result["episodes"])
-      end
-        animes
+  def edit_data(name)
+    animes = []
+    #edit it down to desired information and save it
+    data = JSON.parse(self.get_data(name))
+    results = data.fetch("results")
+    results.each do |result|
+    animes <<  Anime.new(:mal_id => result["mal_id"], :title => result["title"], :rated => result["rated"], :synopsis => result["synopsis"], :url => result["url"], :score => result["score"], :image_url => result["image_url"],:airing=> result["airing"], :episodes => result["episodes"])
     end
+      animes
+  end
 end
