972529887d34e534122ef026173357f861ff3bad
rylandgrounds <rylandgrounds@gmail.com>
2020-07-08 20:43:19 -0500

    deploy prep

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,11 @@
 ENV['SINATRA_ENV'] ||= "development"
-require_all 'app'
 require 'open-uri'
 require 'net/http'
 require 'json'
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 Dotenv.load if ENV['SINATRA_ENV'] == "development"
-#ActiveRecord::Base.establish_connection(
- # :adapter => "sqlite3",
-  #:database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-#)
+
 
 set :database_file, "./database.yml"
 require './app/controllers/application_controller'
